5897904cd7340494bb3d519e92471c2e54b8b1b2
jgmccormick <john.g.mccormick@outlook.com>
2025-10-26 23:38:05 -0400

    added clock tick and framerate limiter

--- main.py
+++ main.py
@@ -5,15 +5,20 @@ from constants import *
 def main():
     pygame.init()
     print("Starting Asteroids!")
-    #print(f"Screen width: {SCREEN_WIDTH}")
-    #print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(000, rect=None)
+
+        screen.fill("black")
         pygame.display.flip()
+        
+        #limits the framerate  to 60 fps
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
