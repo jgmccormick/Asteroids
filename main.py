c2454f8a21a607adf42db45ad0dce13b1d77af3a
jgmccormick <john.g.mccormick@outlook.com>
2025-11-11 12:53:34 -0500

    Added clock variable and clock tick for pygame function

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import pygame
-from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
 
 def main():
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         log_state()
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
@@ -18,6 +18,9 @@ def main():
         screen.fill("black")
         pygame.display.flip()
 
+        #limits the framerate to 60 fps
+        dt = clock.tick(60)/1000
+            
 
 if __name__ == "__main__":
     main()
